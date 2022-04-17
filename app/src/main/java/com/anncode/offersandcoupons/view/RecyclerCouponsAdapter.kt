b2373fba04f9423c0e38fa27d50ec7c4ee7ae3fa
CESCO <cescovalledev@gmail.com>
2022-04-17 00:33:17 -0600

    Hecho pero no entendido x2

--- app/src/main/java/com/anncode/offersandcoupons/view/RecyclerCouponsAdapter.kt
+++ app/src/main/java/com/anncode/offersandcoupons/view/RecyclerCouponsAdapter.kt
@@ -12,7 +12,7 @@ import com.anncode.offersandcoupons.model.Coupon
 import com.anncode.offersandcoupons.R
 import com.squareup.picasso.Picasso
 
-class RecyclerCouponsAdapter(var coupons : ArrayList<Coupon>, var resource: Int) : RecyclerView.Adapter<RecyclerCouponsAdapter.CardCouponHolder>() {
+class RecyclerCouponsAdapter(var coupons : ArrayList<Coupon>?, var resource: Int) : RecyclerView.Adapter<RecyclerCouponsAdapter.CardCouponHolder>() {
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): CardCouponHolder {
         var view: View = LayoutInflater.from(p0!!.context).inflate(resource, p0, false)
@@ -20,11 +20,11 @@ class RecyclerCouponsAdapter(var coupons : ArrayList<Coupon>, var resource: Int)
     }
 
     override fun getItemCount(): Int {
-        return coupons.size
+        return coupons?.size ?: 0
     }
 
     override fun onBindViewHolder(p0: CardCouponHolder, p1: Int) {
-        var coupon = coupons.get(p1)
+        var coupon = coupons?.get(p1)
         p0.setDataCard(coupon)
     }
 
@@ -41,17 +41,17 @@ class RecyclerCouponsAdapter(var coupons : ArrayList<Coupon>, var resource: Int)
             v.setOnClickListener(this)
         }
 
-        fun setDataCard(coupon: Coupon){
+        fun setDataCard(coupon: Coupon?){
             this.coupon = coupon
-            if (coupon.image_url.equals("")){
+            if (coupon?.image_url.equals("")){
                 imgCoupon.setImageResource(R.drawable.header)
             }else{
-                Picasso.get().load(coupon.image_url).resize(520, 520).centerCrop().into(imgCoupon)
+                Picasso.get().load(coupon?.image_url).resize(520, 520).centerCrop().into(imgCoupon)
             }
-            tvTitle.setText(coupon.title)
-            tvDescriptionShort.setText(coupon.descriptionShort)
-            tvCategory.setText(coupon.category)
-            tvDate.setText(coupon.endDate)
+            tvTitle.setText(coupon?.title)
+            tvDescriptionShort.setText(coupon?.descriptionShort)
+            tvCategory.setText(coupon?.category)
+            tvDate.setText(coupon?.endDate)
 
         }
 
